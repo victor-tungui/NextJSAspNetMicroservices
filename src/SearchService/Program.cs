798a8440e328fd056374f66e424a5e9212b05715
Victor H Tungui <capishy@gmail.com>
2024-11-20 20:30:13 -0600

    completed section about docker multiple projects or services

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -19,6 +19,12 @@ builder.Services.AddMassTransit(m =>
 
     m.UsingRabbitMq((context, config) => 
     {
+         config.Host(builder.Configuration["RabbitMq:Host"], "/", host => 
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+
         config.ReceiveEndpoint("search-auction-created", e => {
             e.UseMessageRetry(r => r.Interval(5, 5));
             e.ConfigureConsumer<AuctionCreatedConsumer>(context);
